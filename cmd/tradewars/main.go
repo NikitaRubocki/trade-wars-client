4fb5c27f5a1c7404284093e592d36c3a38c3d2c7
Chevelle Boyer <chevelleboyer@gmail.com>
2019-11-17 11:16:37 -0800

    Move main from scratch.go into main.go

--- cmd/tradewars/main.go
+++ cmd/tradewars/main.go
@@ -3,6 +3,18 @@
 
 package main
 
-// func main () {
+import (
+    "log"
+    "net/http"
+)
 
-// }
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", home)
+    mux.HandleFunc("/snippet", showSnippet)
+    mux.HandleFunc("/snippet/create", createSnippet)
+
+    log.Println("Starting server on :8088")
+    err := http.ListenAndServe(":8088", mux)
+    log.Fatal(err)
+}
