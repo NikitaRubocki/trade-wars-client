50ab8aa349ab27f7dd50c3ccbc7938f71974e2e0
Chevelle Boyer <chevelleboyer@gmail.com>
2019-11-19 19:58:49 -0800

    Change port to 80 for debugging

--- cmd/tradewars/main.go
+++ cmd/tradewars/main.go
@@ -19,7 +19,7 @@ func main() {
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    log.Println("Starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
+    log.Println("Starting server on :80")
+    err := http.ListenAndServe(":80", mux)
     log.Fatal(err)
 }
