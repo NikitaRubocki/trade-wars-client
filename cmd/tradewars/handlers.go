e86e0cdd22970ade23b9d1d54ad65d82331ae9ed
Nikita Rubocki <nikita.rubocki@gmail.com>
2019-11-23 15:32:24 -0800

    Post request working

--- cmd/tradewars/handlers.go
+++ cmd/tradewars/handlers.go
@@ -8,38 +8,43 @@ import (
 )
 
 func welcome(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("inside welcome")
 	if r.Method != http.MethodGet && r.Method != http.MethodPost {
         http.Error(w, "Method Not Allowed", 405)
         return
     }
 
-    if r.Method == http.MethodPost {
+    fmt.Println("r.Method: ", r.Method)
+    if r.Method == http.MethodGet {
+        ts, err := template.ParseFiles("./ui/web/welcome.html")
+        if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+        }
+
+        err = ts.Execute(w, nil)
+        if err != nil {
+            log.Println(err.Error())
+            http.Error(w, "Internal Server Error", 500)
+        }
+    }else if r.Method == http.MethodPost {
+        fmt.Println("inside post if")
         err := r.ParseForm()
         if err != nil {
             log.Println(err.Error())
             http.Error(w, "Internal Server Error", 500)
         }
         callsign := r.Form.Get("callsign")
+        
         fmt.Println(callsign)
     }
 
     
-
-    ts, err := template.ParseFiles("./ui/web/welcome.html")
-    if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-        return
-    }
-
-    err = ts.Execute(w, nil)
-    if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-    }
 }
 
 func starMap(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("inside starmap")
 	ts, err := template.ParseFiles("./ui/web/navigationscreen.html")
     if err != nil {
         log.Println(err.Error())
