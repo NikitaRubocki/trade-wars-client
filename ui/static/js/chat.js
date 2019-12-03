3a3680edbc8696f5c78b2d59a105279d55e6efc1
Nikita Rubocki <nikita.rubocki@gmail.com>
2019-12-02 21:24:13 -0800

    Javascipt still not working, trying to get usermsg to show up in chatbox Many commented out sections, this commit is just to show that I was trying gosh darn it

--- ui/static/js/chat.js
+++ ui/static/js/chat.js
@@ -1,5 +1,12 @@
-function hello() {
-    alert("hello")
+function sendMsg() {
+    var chatLog = document.getElementById("chatbox").innerHTML;
+    var msg = document.getElementById("usermsg").value;
+    if (msg != '') {
+        // alert(msg);
+        // alert(chatLog);
+        // alert(chatLog+"\n"+msg);
+        chatLog = msg;
+    }
 }
     
 
